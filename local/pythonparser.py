47bfc28d53dd8ab91bca0dd07c60168a0e4cbc19
R3GAL0 <maxfwalent@gmail.com>
2026-02-17 17:06:30 -0500

    fixed an issue where the diff of raw_logs and output_logs didnt provide the correct output

--- local/pythonparser.py
+++ local/pythonparser.py
@@ -16,26 +16,32 @@ output_path = '/home/r3gal/develop/mtga_pipeline/data/upload'
 raw_logs = os.listdir(path=raw_path)
 output_logs = os.listdir(path=output_path)
 
-output_files = [x[10:] for x in output_logs]
+# removing the Filtered_ prefix and .csv suffix, adding .log suffix
+output_files = [x[9:-4]+'.log' for x in output_logs]
 
 if 'placeholder.md' in raw_logs:
     raw_logs.remove('placeholder.md')
-    
-difference = list(set(raw_logs) - set(output_files))
 
+# removing the already run log files from the list of raw_logs to run
+for item in output_files:
+    if item in raw_logs:
+        raw_logs.remove(item)
 
 
 def parse_logs(input_path_file, output_path_file):
     recording = False
     game_num = 1
+    print(input_path_file)
 
     with open(input_path_file, 'r') as log_data, open(output_path_file, 'w') as csvfile:
         out_file = csv.writer(csvfile)
         out_file.writerow(["game_num", "metadata", "payload"])
 
         for line in log_data:
-
-            if recording and line.startswith('[UnityCrossThreadLogger]'):    
+            # skip these system messages
+            unwanted = ['ClientToGreuimessage', 'ClientToGremessage', '==>', 'Client.TcpConnection.Close']
+            payload = ''
+            if recording and line.startswith('[UnityCrossThreadLogger]') and not any(u in line for u in unwanted):    
                 # Dropping '[UnityCrossThreadLogger]'
                 line = line[24:]
 
@@ -67,13 +73,15 @@ def parse_logs(input_path_file, output_path_file):
             # Looking for the start and stop of the match
             if line.startswith('[UnityCrossThreadLogger]STATE CHANGED {"old":"ConnectedToMatchDoor_ConnectedToGRE_Waiting","new":"Playing"}'):
                 recording = True
-            elif line.startswith('[UnityCrossThreadLogger]STATE CHANGED {"old":"Playing","new":"MatchCompleted"}'):
+                print('recording on')
+            elif line.startswith('[UnityCrossThreadLogger]STATE CHANGED {"old":"Playing","new":"MatchCompleted"}' or payload == '{"old":"Playing","new":"MatchCompleted"}'):
                 recording = False
+                print('recording off')
                 game_num += 1
 
 
-for raw_file in difference:
-    raw_path_file       = raw_path + r'/' + raw_file
-    output_path_file    = output_path + r'/Filtered_' + raw_file[:-4] + '.csv'
+for file in raw_logs:
+    raw_path_file       = raw_path + r'/' + file
+    output_path_file    = output_path + r'/Filtered_' + file[:-4] + '.csv'
 
     parse_logs(raw_path_file, output_path_file)
