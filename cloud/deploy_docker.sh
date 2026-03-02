44ffb56a4bd850c07820367d304154b13b4bb8d2
R3GAL0 <maxfwalent@gmail.com>
2026-03-02 09:44:18 -0500

    docker and gcp are configured. Working through deployment with cloud run jobs

---
+++ cloud/deploy_docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euo pipefail
+
+# build the image
+docker build -t gcr.io/mtgapipeline/mtga-pipeline:latest .
+# tag and push the image to gcp
+docker tag gcr.io/mtgapipeline/mtga-pipeline:latest northamerica-northeast2-docker.pkg.dev/mtgapipeline/cloud-run-source-deploy/mtga-pipeline:latest
+docker push northamerica-northeast2-docker.pkg.dev/mtgapipeline/cloud-run-source-deploy/mtga-pipeline:latest
+
+# Update the GCP cloud run job and execute
+gcloud run jobs update mtga-pipeline-job \
+--image northamerica-northeast2-docker.pkg.dev/mtgapipeline/cloud-run-source-deploy/mtga-pipeline:latest \
+--region northamerica-northeast2
+gcloud run jobs execute mtga-pipeline-job --region northamerica-northeast2
