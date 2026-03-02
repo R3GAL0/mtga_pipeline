cb69364e6bc3b9dfebce773359a9b08cf8d8cada
R3GAL0 <maxfwalent@gmail.com>
2026-03-02 14:07:00 -0500

    set schema for bigquery silver layer in GCP

---
+++ cloud/main_duckdb.py
@@ -0,0 +1,55 @@
+# Currently reprocesses all csvs, will need to change later
+
+import os
+from make_tables import insert_all
+from google.cloud import storage
+from show_tables import show_tables
+
+TMP_BASE = "/tmp/data"
+DB_BUCKET_NAME = "mtga_pipeline_bucket"
+DB_FILE = "mtga_local.duckdb"
+LANDING_PREFIX = "landing/"
+
+def main():
+    os.makedirs(TMP_BASE, exist_ok=True)
+
+    client = storage.Client()
+    bucket = client.bucket(DB_BUCKET_NAME)
+
+    # Pull landing/csvs to /tmp/data
+    blobs = client.list_blobs(DB_BUCKET_NAME, prefix=LANDING_PREFIX)
+
+    for blob in blobs:
+        if blob.name.endswith(".csv"):
+            local_path = os.path.join(TMP_BASE, os.path.basename(blob.name))
+            blob.download_to_filename(local_path)
+            print(f"Downloaded {blob.name} to {local_path}")
+
+
+    # Pull duckdb file to /tmp/data
+    db_blob = bucket.blob(DB_FILE)
+    local_db_path = os.path.join(TMP_BASE, DB_FILE)
+
+    if db_blob.exists():
+        db_blob.download_to_filename(local_db_path)
+        print("Downloaded existing DuckDB")
+    else:
+        print("No DuckDB found — will create new")
+
+    # Run process
+    insert_all(data_dir=TMP_BASE, db_dir=local_db_path)
+
+    # show the new row counts
+    show_tables(local_db_path)
+
+    # Push DuckDB back to GCS
+    db_blob.upload_from_filename(local_db_path)
+
+    # Clean up
+    for f in os.listdir(TMP_BASE):
+        os.remove(os.path.join(TMP_BASE, f))
+    print("ETL job completed.")
+
+
+if __name__ == "__main__":
+    main()
